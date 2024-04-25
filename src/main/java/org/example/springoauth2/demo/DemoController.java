bdbd0ec65e98b4023adfaa5cc45727e74fb29508
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-04-26 00:58:43 +0300

    adding endpoint

--- src/main/java/org/example/springoauth2/demo/DemoController.java
+++ src/main/java/org/example/springoauth2/demo/DemoController.java
@@ -1,12 +1,14 @@
 package org.example.springoauth2.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/demo")
 public class DemoController {
+    @GetMapping
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello from security endpoint");
     }
