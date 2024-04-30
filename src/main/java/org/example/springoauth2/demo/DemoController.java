006568ac2e6cea413c42428f07c0b8654c933f0c
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-04-30 15:28:59 +0300

    getting logged user name

--- src/main/java/org/example/springoauth2/demo/DemoController.java
+++ src/main/java/org/example/springoauth2/demo/DemoController.java
@@ -1,6 +1,8 @@
 package org.example.springoauth2.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/demo")
 public class DemoController {
     @GetMapping
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello from security endpoint");
+    public ResponseEntity<String> hello(@AuthenticationPrincipal OAuth2User principal) {
+        return ResponseEntity.ok("Hello " + principal.getAttribute("name"));
     }
 }
